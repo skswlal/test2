49da21008a3b037f8b9ff0cd622f0d8c0f16b084
Jimi Kim <jimik0107@gmail.com>
2025-05-22 21:35:04 +0200

    .

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Sensor Dashboard</h1>
+      <p>Welcome! The API is running. Try /api/sensor-data for data...</p>
+    </main>
+  );
+}
